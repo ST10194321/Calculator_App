e7a63420caf6a80ac55402ca3b429d7bfa843c2e
Jose Gonzalves <st10194321@vcconnect.edu.za>
2025-02-27 08:22:10 +0200

    Calc App - Firebase changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.firebase.auth)
+
+    implementation("com.google.firebase:firebase-auth:22.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
